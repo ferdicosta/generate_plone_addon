faed152ba916b67d201a1d8aaa013fc3090d34b2
federico <Shylock92!16>
2026-04-02 13:34:02 +0200

    v0.8.1

--- src/generate_plone_addon/template_theme/src/{{namespace}}/{{module}}/browser/interfaces_view.py
+++ src/generate_plone_addon/template_theme/src/{{namespace}}/{{module}}/browser/interfaces_view.py
@@ -2,7 +2,7 @@ import logging
 
 from Products.Five import BrowserView
 from Products.Five.utilities.interfaces import IMarkerInterfaces
-from cis.site.interfaces import CustomViewInterface
+from {{package_name}} import CustomViewInterface
 from zope.interface import Interface
 
 logger = logging.getLogger(__name__)
